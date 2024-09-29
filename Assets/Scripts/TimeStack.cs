b3c9fb84fc91684f80189d542bed26f3bd4a23d8
Alexander Betancourt <AlexanderBBetancourt@proton.me>
2024-09-29 00:06:23 -0700

    Work was done on scripts

--- Assets/Scripts/TimeStack.cs
+++ Assets/Scripts/TimeStack.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class TimeStack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // So essentually this will allow us to go foward and backwards. 
+    private Stack<Action> _futureActions;
+    // The current action is on the top of the stack.. 
+    private Stack<Action> _previousActions;
+
+    public void addAction(Action newAction)
     {
-        
+        _previousActions.Push(newAction);
     }
+    public Action getPrevAction()
+    {
+        if (_previousActions.Count == 0)
+        {
+            // Just return null if nothing is in the stack
+            return null;
+        }
+        return _previousActions.Peek();
+    }
+    public Action revertAction()
+    {
+        // Gets the previous and pushes it to the future stack.. 
+        _futureActions.Push(_previousActions.Peek());
+        if (_previousActions.TryPop(out var input))
+        {
+            return input;
+        }
+        return null;
+    }
+
 }
