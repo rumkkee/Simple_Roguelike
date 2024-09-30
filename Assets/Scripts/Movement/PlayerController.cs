a9df06320d3afa721b1f2f897c47638438bb17cf
Alexander Betancourt <AlexanderBBetancourt@proton.me>
2024-09-30 04:54:24 -0700

    Controller support done

--- Assets/Scripts/Movement/PlayerController.cs
+++ Assets/Scripts/Movement/PlayerController.cs
@@ -1,47 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
+    [Tooltip("How fast a player moves")]
+    public float moveSpeed = 5f;
     private PlayerMovement _controls;
+    private InputAction _move;
+    private bool _isMoving;
+    private Vector3 _targetPos;
+    // Tilemap stuff.. 
+    [Header("Tilemap collisions")]
+    [SerializeField]
+    [Tooltip("The tile map that the ground is on")]
+    private Tilemap _groundTileMap;
 
     [SerializeField]
-    private Tilemap _groundTitleMap;
-
-    [SerializeField]
+    [Tooltip("The tile map that the collisions are determined is on")]
     private Tilemap _collisionTileMap;
     private void Awake()
     {
         _controls = new PlayerMovement();
+        _move = _controls.Main.Movement;
     }
     private void OnEnable()
     {
-        _controls.Enable();
+        _move.Enable();
     }
     private void OnDisable()
     {
-        _controls.Disable();
+        _move.Disable();
     }
-    void Start()
+    void Update()
     {
-        _controls.Main.Movement.performed += ctx => move(ctx.ReadValue<Vector2>());
+        if (!_isMoving)
+        {
+            Vector2 moveVec = _move.ReadValue<Vector2>();
+
+            if (moveVec.sqrMagnitude > 0.1f)
+            {
+                Vector3 direction = new Vector3(moveVec.x, moveVec.y, 0);
+                move(direction);
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPos, moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, _targetPos) < float.Epsilon)
+            {
+                _isMoving = false;
+            }
+        }
     }
 
     public void move(Vector2 direction)
     {
-        if (!canMove(direction))
+        if (canMove(direction))
         {
-            return;
+            _targetPos = transform.position + (Vector3)direction;
+            _isMoving = true;
         }
-        transform.position += (Vector3)direction;
     }
-
     public bool canMove(Vector2 direction)
     {
-        Vector3Int gridPos = _groundTitleMap.WorldToCell(transform.position + (Vector3)direction);
-        if (!_groundTitleMap.HasTile(gridPos) || _collisionTileMap.HasTile(gridPos))
+        Vector3Int gridPos = _groundTileMap.WorldToCell(transform.position + (Vector3)direction);
+        if (!_groundTileMap.HasTile(gridPos) || _collisionTileMap.HasTile(gridPos))
         {
             return false;
         }
