8741f1e92931fe4e8125ea11e7be9522021d59a6
Arturo C-Zacarias <accesareo@gmail.com>
2024-10-06 21:41:46 -0700

    RoomNode Generation complete
    * obtains grid positions offset by 1
    * checks and prevents duplicate positions

--- Assets/Scripts/Room/FloorGenerator.cs
+++ Assets/Scripts/Room/FloorGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,26 +8,91 @@ public class FloorGenerator : MonoBehaviour
 {
     // Have a reference to floor resources, room nodes
 
+    private List<RoomNode> roomNodes;
+
     [SerializeField] private FloorResources floorResources;
-    private GameObject floorsContainer;
+    [SerializeField]private GameObject floorsContainer;
 
     
     [SerializeField] private int roomCount; // Number of rooms to generate, apart from starting room.
 
-    public void StartRoomGeneration()
+    // Generates the placements for rooms
+
+    // Test Start Script
+    private void Start()
     {
-        GameObject startingRoom = Instantiate(gameObject, floorsContainer.transform.position, Quaternion.identity, floorsContainer.transform);
+        GenerateNodes();
+    }
+    public void GenerateNodes()
+    {
+        roomNodes = new List<RoomNode>();
+
+        RoomNode startingNode = new RoomNode();
+        startingNode.gridPos = Vector2.zero;
+
+        roomNodes.Add(startingNode);
+        Room newRoom = Instantiate(floorResources.startingRoomPrefab, Vector2.zero, Quaternion.identity, floorsContainer.transform);
 
         int roomsGeneratedCount = 0;
 
         while (roomsGeneratedCount < roomCount)
         {
-            // TODO: Get a random direction
+            RoomNode nodeSpawnedFrom = roomNodes[Random.Range(0, roomNodes.Count)];
 
+            int randWallPos = Random.Range(0, 4);
 
+            Vector2 gridPosShift = Vector2.zero;
+            if (randWallPos == 0) 
+            { 
+                gridPosShift = Vector2.up; 
+            } 
+            else if (randWallPos == 1) 
+            { 
+                gridPosShift = Vector2.right; 
+            } 
+            else if (randWallPos == 2) 
+            { 
+                gridPosShift = Vector2.down; 
+            } 
+            else 
+            { 
+                gridPosShift = Vector2.left; 
+            } 
+
+            Vector2 newNodePos = nodeSpawnedFrom.gridPos + gridPosShift;
+
+            if (NodeExistsAtGridPos(newNodePos))
+            {
+                continue;
+            }
+
+            RoomNode roomNode = new RoomNode();
+            roomNode.gridPos = newNodePos;
+            roomNodes.Add(roomNode);
+
+            Debug.Log(newNodePos.x + ", " + newNodePos.y);
+            newRoom = Instantiate(floorResources.startingRoomPrefab, newNodePos * 11, Quaternion.identity, floorsContainer.transform);
 
             roomsGeneratedCount++;
         }
+    }
+
+    private bool NodeExistsAtGridPos(Vector2 gridPos)
+    {
+        foreach (RoomNode roomNode in roomNodes) 
+        { 
+            if (roomNode.gridPos == gridPos) 
+            {  
+                return true; 
+            } 
+        }
+        return false;
+    }
+
+    // // Places rooms at each room node
+    public void GenerateRooms()
+    {
+
     }
 
 }
