6d02a0b2ca43949f39841c7dfd40bd593202ec5a
Kyle Lynn <lynninlcn@gmail.com>
2024-10-10 02:17:13 -0700

    Added player stats display and exit shop script

--- Assets/Scripts/Shop Scripts/BackToDungeon.cs
+++ Assets/Scripts/Shop Scripts/BackToDungeon.cs
@@ -16,6 +16,7 @@ public class TransitionToShop : MonoBehaviour
     public string shopSceneName = "Shop Scene";
     public float delayBeforeTrans = 1f; //time in seconds before trans
     // Start is called before the first frame update
+    
     public void GoToShop()
     {
         StartCoroutine(transitionToShop());
@@ -27,5 +28,15 @@ public class TransitionToShop : MonoBehaviour
         yield return new WaitForSeconds(delayBeforeTrans);
         SceneManager.LoadScene(shopSceneName);
     }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("entering shop");
+            GoToShop(); 
+        }
+    }
+    
     
 }
