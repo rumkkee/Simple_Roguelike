44a4463533c7c7a650b4eac44bb45b93e326739a
Arturo C-Zacarias <accesareo@gmail.com>
2024-10-14 00:59:38 -0700

    Updated Room Prefabs

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Room : MonoBehaviour
 {
-    
+    public Tilemap groundTilemap;
+    public Tilemap collisionTilemap;
+    public Tilemap doorTilemap;
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Set the player's grids to use this room's grids.
+        PlayerMovement player = collider.GetComponent<PlayerMovement>();
+        if(player != null)
+        {
+            Debug.Log("Player entered room trigger, setting tilemaps!");
+            player.doorTileMap = doorTilemap;
+            player.collisionTileMap = collisionTilemap;
+            player.groundTileMap = groundTilemap;
+        }
+    }
 }
