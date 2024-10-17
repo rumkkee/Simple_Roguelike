8d9f707e36a0cc9c8d9fc696bf0dea3477b45a69
Alexander Betancourt <AlexanderBBetancourt@proton.me>
2024-10-16 19:39:38 -0700

    Save Manger is up and running

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
+[Serializable]
 // This exists for saving
 [CreateAssetMenu(fileName = "playerStatsObject", menuName = "Player Stats Object")]
 public class PlayerStats : ScriptableObject
 {
+    private const int NUM_OF_ITEMS = 12;
     // Exists for stats reasons
     public static long totalCurrency;
     // Again stats reasons
@@ -29,10 +33,54 @@ public class PlayerStats : ScriptableObject
 
     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     // Items list for storing items.
+
+    // For now, we are ignoring items.. 
     public List<HealthItem> healthItems = new List<HealthItem>();
     public List<DamageItem> damageItems = new List<DamageItem>();
     public List<ArmorItem> armorItems = new List<ArmorItem>();
     public List<SpeedItem> speedItems = new List<SpeedItem>();
 
+    public string Serialize()
+    {
+        StringBuilder retval = new StringBuilder();
+        
+        retval.Append(totalCurrency).Append(",")          // Append each field with a separator (comma)
+              .Append(totalStepsTaken).Append(",")
+              .Append(stepsAvaliable).Append(",")
+              .Append(health).Append(",")
+              .Append(startingHealth).Append(",")
+              .Append(armorValue).Append(",")
+              .Append(startingArmor).Append(",")
+              .Append(startingAttack).Append(",")
+              .Append(startingSpeed).Append(",")
+              .Append(startingCurrency).Append(",")
+              .Append(startingStepsTaken).Append(",")
+              .Append(startingStepsAvailable);
 
+        return retval.ToString();  // Return the concatenated string
+    }
+
+    public void Deserialize(string serializedData)
+    {
+        string[] values = serializedData.Split(',');
+
+        if (values.Length < NUM_OF_ITEMS)
+        {
+            Debug.LogError($"Data length mismatch: expected {NUM_OF_ITEMS} elements.");
+            return;
+        }
+
+        totalCurrency = long.Parse(values[0]);
+        totalStepsTaken = long.Parse(values[1]);
+        stepsAvaliable = int.Parse(values[2]);
+        health = int.Parse(values[3]);
+        startingHealth = int.Parse(values[4]);
+        armorValue = int.Parse(values[5]);
+        startingArmor = int.Parse(values[6]);
+        startingAttack = int.Parse(values[7]);
+        startingSpeed = int.Parse(values[8]);
+        startingCurrency = double.Parse(values[9]);
+        startingStepsTaken = int.Parse(values[10]);
+        startingStepsAvailable = int.Parse(values[11]);
+    }
 }
