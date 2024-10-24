4b5ea1f520bb98a57185fde0a18e545fd0deda80
Arturo C-Zacarias <accesareo@gmail.com>
2024-10-24 09:47:45 -0700

    SetDefault button functionality implemented

---
+++ Assets/Scripts/Menus/Options.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    public float musicVol;
+    public Language language;
+
+
+    public void SetDefault()
+    {
+        musicVol = 1;
+        language = Language.english;
+    }
+
+}
