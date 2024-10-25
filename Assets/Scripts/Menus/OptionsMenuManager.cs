2be453eba10f63ae7c123cbea194b83bcd160160
Arturo C-Zacarias <accesareo@gmail.com>
2024-10-24 19:52:37 -0700

    Volume slider controls configured

--- Assets/Scripts/Menus/OptionsMenuManager.cs
+++ Assets/Scripts/Menus/OptionsMenuManager.cs
@@ -4,11 +4,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 public class OptionsMenuManager : MonoBehaviour
 {
     public GameObject lastSelectedButton;
     public TextMeshProUGUI languageText;
     public TextMeshProUGUI screenModeText;
+
+    public Slider musicVolumeSlider;
+    public Button musicButton; // Used to return to this button from the volume slider
 
     public void OnExitPressed()
     {
@@ -35,6 +39,25 @@ public class OptionsMenuManager : MonoBehaviour
         {
             Screen.fullScreen = true;
             screenModeText.text = "Full Screen";
+        }
+    }
+
+    public void OnMusicVolumeButtonPressed()
+    {
+        MenuManager.instance.eventSystem.SetSelectedGameObject(musicVolumeSlider.gameObject);
+        StartCoroutine(OnSliderBeingControlled());
+    }
+
+    private IEnumerator OnSliderBeingControlled()
+    {
+        while(MenuManager.instance.eventSystem.currentSelectedGameObject == musicVolumeSlider.gameObject)
+        {
+            yield return null;
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                MenuManager.instance.eventSystem.SetSelectedGameObject(musicButton.gameObject);
+                yield break;
+            }
         }
     }
 }
