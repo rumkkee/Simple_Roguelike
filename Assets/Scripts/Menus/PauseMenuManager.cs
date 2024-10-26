7316e179c6fd9a5f4da4af1c452faa49f9d298b5
Arturo C-Zacarias <accesareo@gmail.com>
2024-10-26 00:05:28 -0700

    Exit button implemented

--- Assets/Scripts/Menus/PauseMenuManager.cs
+++ Assets/Scripts/Menus/PauseMenuManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PauseMenuManager instance;
+
+    public GameObject pauseMenuPanel;
+
+    private void Awake()
     {
         
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnContinuePressed()
     {
-        
+        // Call the game time to resume, if paused
+        pauseMenuPanel.SetActive(false);
     }
+
+    public void OnSavePressed()
+    {
+
+    }
+
+    public void OnSettingsPressed()
+    {
+
+    }
+
+    public void OnExitToMainPressed()
+    {
+        pauseMenuPanel.SetActive(false);
+        SceneManager.LoadScene(Scenes.instance.mainMenuScene);
+    }
+
+
 }
