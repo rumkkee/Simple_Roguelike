b684b9c20407aa5f121abc8593c364c9d748fb5b
Alexander Betancourt <AlexanderBBetancourt@proton.me>
2024-10-27 00:00:18 -0700

    PartcleManager

--- Assets/Scripts/PartcleManager.cs
+++ Assets/Scripts/PartcleManager.cs
@@ -4,5 +4,67 @@ using UnityEngine;
 
 public class PartcleManager : MonoBehaviour
 {
+    [HideInInspector]
+    public static PartcleManager instance;
+    public enum PartcleType
+    {
+        Blood,
+        DustLeft,
+        DustRight,
+        Treasure,
+    }
+    public GameObject[] partcles;
+    public void Awake()
+    {
+        // check for any other inst. 
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        // Okay then init
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
+    public void makePartcleFX(PartcleType type, GameObject parent)
+    {
+        switch (type)
+        {
+            case PartcleType.Blood:
+                Instantiate(partcles[(int)PartcleType.Blood], parent.transform);
+                break;
+            case PartcleType.DustLeft:
+                Instantiate(partcles[(int)PartcleType.DustLeft], parent.transform);
+                break;
+            case PartcleType.DustRight:
+                Instantiate(partcles[(int)PartcleType.DustRight], parent.transform);
+                break;
+            case PartcleType.Treasure:
+                Instantiate(partcles[(int)PartcleType.Treasure], parent.transform);
+                break;
+            default:
+                break;
+        }
+    }
+    public void makePartcleFX(PartcleType type, Transform pos)
+    {
+        switch (type)
+        {
+            case PartcleType.Blood:
+                Instantiate(partcles[(int)PartcleType.Blood], pos, true);
+                break;
+            case PartcleType.DustLeft:
+                Instantiate(partcles[(int)PartcleType.DustLeft], pos, true);
+                break;
+            case PartcleType.DustRight:
+                Instantiate(partcles[(int)PartcleType.DustRight], pos, true);
+                break;
+            case PartcleType.Treasure:
+                Instantiate(partcles[(int)PartcleType.Treasure], pos, true);
+                break;
+            default:
+                break;
+        }
+    }
 }
---
+++ Assets/Scripts/PlayerOrientationManger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOrientationManger : MonoBehaviour
+{
+    public GameObject leftSprite;
+    public GameObject rightSprite;
+    public GameObject leftDust;
+    public GameObject rightDust;
+    public bool isLeft = false;
+    public void swtichOrientation(Vector2 dir) {
+        if(dir == Vector2.left) {
+            rightSprite.SetActive(true);
+            leftSprite.SetActive(false);
+            isLeft = false;
+        } else if(dir == Vector2.right) {
+            leftSprite.SetActive(true);
+            rightSprite.SetActive(false);
+            isLeft = true;
+        }
+    }
+}
